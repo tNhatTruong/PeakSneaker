ef93b04239fdd7c7fc8740c286e52b96abf91cc3
nTruong257 <22130306@st.hcmuaf.edu.vn>
2026-06-06 12:56:12 +0700

    feat(backend): create brand entity and repository
    Khởi tạo Entity Brand ánh xạ với bảng brands trong Database và thiết lập BrandRepository.

---
+++ backend/src/main/java/com/peaksneaker/repository/BrandRepository.java
@@ -0,0 +1,12 @@
+package com.peaksneaker.repository;
+
+import com.peaksneaker.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+    List<Brand> findByIsDeletedFalse();
+}
